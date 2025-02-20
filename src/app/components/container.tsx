1c97d1e22e24a428d63d3b44f2e472f35402e356
sahlbakshi <sahlaajazbakshi@gmail.com>
2025-02-20 04:58:39 -0500

    small changes

--- src/app/components/container.tsx
+++ src/app/components/container.tsx
@@ -1,6 +1,6 @@
 export default function Container({ children }: { children: React.ReactNode }) {
     return (
-      <div className="flex justify-center my-10 sm:my-14 mx-8">
+      <div className="flex justify-center my-8 sm:my-14 mx-8">
         <div className="max-w-[600px] w-full flex flex-col gap-6">
           {children}
         </div>

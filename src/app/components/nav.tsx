98c28b7844ef02f6bf1b7cbd2dc01d85a18e3060
sahlbakshi <sahlaajazbakshi@gmail.com>
2025-06-15 23:26:37 -0400

    complete

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -7,8 +7,8 @@ interface NavbarProps {
 export default function Navbar({ page }: NavbarProps) {
 
   return (
-    <div className="w-full flex justify-between items-start text-[15px]">
-      <div>
+    <div className="w-full sm:flex justify-between items-start text-[15px]">
+      <div className="sm:mb-0 mb-4">
         <Link href={'/'} className={page === "home" ? "" : "text-linkGray"}>Sahl Bakshi</Link>
         <div className="text-linkGray">Updated Jun 10, 2025</div>
       </div>
@@ -16,7 +16,7 @@ export default function Navbar({ page }: NavbarProps) {
       <div className="flex gap-4">
           <Link className="text-linkBlue underline" href={'https://www.linkedin.com/in/sahl-bakshi/'}>LinkedIn</Link>
           <Link className="text-linkBlue underline" href={'https://github.com/sahlbakshi'}>GitHub</Link>
-          <Link className="text-linkBlue underline" href={'https://x.com/sahlbakshi'}>Twitter</Link>
+          <Link className="text-linkBlue underline" href={'https://x.com/sahlbakshi'}>X/Twitter</Link>
       </div>
       {
         /*

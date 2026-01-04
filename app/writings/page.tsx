73755340565ef9d25aa1e5d8a70735a48d9acc6c
sahlbakshi <sahlaajazbakshi@gmail.com>
2026-01-04 07:34:28 -0500

    rewrite

--- app/writings/page.tsx
+++ app/writings/page.tsx
@@ -9,7 +9,7 @@ export default function Writings() {
           <p>Jan 4, 2026</p>
         </li>
         <li className="flex flex-row gap-2">
-          <ParaLink link="People Who Inspire Me" href="https://docs.google.com/document/d/1m3z4T0-7Za3d14V4GXy5P_k_dl8T68csrpmvxMmhyDI/preview"/>
+          <ParaLink link="People Who Inspire Me" href="https://docs.google.com/document/d/11AO15SP_tn80WtyRv4EVT4093cr2qj1BkYZhONwlfFw/preview"/>
           <p>Jan 4, 2026</p>
         </li>
       </ul>

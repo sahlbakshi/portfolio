c32cb3258804ecea13a24a8d7b3d3c1d027d3e89
sahlbakshi <sahlaajazbakshi@gmail.com>
2026-01-04 05:19:27 -0500

    rewrite

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -3,7 +3,7 @@ import ParaLink from "@/components/paralink";
 export default function Favorites() {
   return (
     <div className="flex flex-col gap-6">
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-1">
         <p className="font-medium">Movies</p>
         <ParaLink link="Little Miss Sunshine" href="https://www.rottentomatoes.com/m/little_miss_sunshine"/>
         <ParaLink link="Whiplash" href="https://www.rottentomatoes.com/m/whiplash_2014"/>
@@ -13,7 +13,7 @@ export default function Favorites() {
         <ParaLink link="Homebound" href="https://www.rottentomatoes.com/m/homebound_2025"/>
         <ParaLink link="3 Idiots" href="https://www.rottentomatoes.com/m/3_idiots"/>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-1">
         <p className="font-medium">TV Shows</p>
         <ParaLink link="Breaking Bad" href="https://www.rottentomatoes.com/tv/breaking_bad"/>
         <ParaLink link="Invincible" href="https://www.rottentomatoes.com/tv/invincible"/>
--- app/writings/page.tsx
+++ app/writings/page.tsx
@@ -3,7 +3,7 @@ import ParaLink from "@/components/paralink";
 export default function Writings() {
   return (
     <div>
-      <ul>
+      <ul className="flex flex-col gap-1">
         <li className="flex flex-row gap-2">
           <ParaLink link="What Moneyball Taught Me" href="https://docs.google.com/document/d/1m3z4T0-7Za3d14V4GXy5P_k_dl8T68csrpmvxMmhyDI/preview"/>
           <p>Jan 4, 2026</p>

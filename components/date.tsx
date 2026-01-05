494d7537ec0ae44f083c77137ae8839403b82500
sahlbakshi <sahlaajazbakshi@gmail.com>
2026-01-04 22:19:59 -0500

    added date

---
+++ components/date.tsx
@@ -0,0 +1,11 @@
+export default function Date({
+  children,
+}: {
+  children?: React.ReactNode;
+}) {
+  return (
+    <p className="text-[#8A8A8A] mb-4">
+      {children}
+    </p>
+  );
+}
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,13 +1,15 @@
 import type { MDXComponents } from 'mdx/types'
 import ParaLink from "@/components/paralink";
 import Footer from './components/footer';
+import Date from './components/date';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ParaLink,
     Footer,
+    Date,
     h1: ({ children }) => (
-      <h1 className="font-semibold text-[20px] mb-5 sm:-mt-1">{children}</h1>
+      <h1 className="font-semibold text-[19px] mt-1 sm:-mt-0.5">{children}</h1>
     ),
     p: ({ children }) => (
       <p className="mb-5">{children}</p>

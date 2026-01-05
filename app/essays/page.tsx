345c250a3ac0c8dca739d3943198fccdae3757c1
sahlbakshi <sahlaajazbakshi@gmail.com>
2026-01-05 12:45:02 -0500

    updates

--- app/essays/page.tsx
+++ app/essays/page.tsx
@@ -2,19 +2,14 @@ import ParaLink from "@/components/paralink";
 
 const essays = [
   {
-    title: "Underdog Founders",
-    href: "/essays/underdog-founders",
-    date: "Jan 05, 2026",
-  },
-  {
     title: "Giving Advice is Flawed",
     href: "/essays/giving-advice-is-flawed",
-    date: "Jan 04, 2026",
+    date: "Dec 29, 2025",
   },
   {
     title: "The Billy Beane Rule",
     href: "/essays/the-billy-beane-rule",
-    date: "Dec 29, 2025",
+    date: "Dec 28, 2025",
   }
 ]
 

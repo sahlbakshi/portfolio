f5211ab4d7a02922c19cf98fb552182f04ed4f26
sahlbakshi <sahlaajazbakshi@gmail.com>
2026-01-19 19:41:47 -0500

    edited font size

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer({
   children?: React.ReactNode;
 }) {
   return (
-    <footer className="mt-10 pt-5 border-t border-black/10 text-[14px] text-black/40 leading-relaxed [&_p]:mb-2">
+    <footer className="mt-10 pt-5 border-t border-black/10 text-[13px] text-black/40 leading-relaxed [&_p]:mb-2">
       {children}
     </footer>
   );

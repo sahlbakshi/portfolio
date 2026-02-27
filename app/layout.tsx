51e11c2c1e811e479948c816410d542a1b6c95fd
sahlbakshi <sahlaajazbakshi@gmail.com>
2026-02-27 08:19:57 -0500

    changed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
+import { Open_Sans, Spectral } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/sidebar";
 
 const open_sans = Open_Sans({
   variable: "--font-open-sans",
   subsets: ["latin"],
+});
+
+const spectral = Spectral({
+  variable: "--font-spectral",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
 export const metadata: Metadata = {
@@ -20,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${open_sans.variable} antialiased`}>
+      <body className={`${spectral.variable} antialiased`}>
         <div className="flex flex-row justify-center items-start">
           <div className="flex flex-col w-full sm:w-auto sm:flex-row m-6 sm:m-16 gap-5 sm:gap-10">
             <Sidebar />

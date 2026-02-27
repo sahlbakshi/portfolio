efaa816355cda21e24c1388463869a48eea3267b
sahlbakshi <sahlaajazbakshi@gmail.com>
2026-02-27 08:55:12 -0500

    added full font

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,14 @@ import "./globals.css";
 import Sidebar from "@/components/sidebar";
 
 const spectral = localFont({
-  src: "../public/Spectral-Regular.ttf",
+  src: [
+    { path: "../public/Spectral-Regular.ttf", weight: "400", style: "normal" },
+    { path: "../public/Spectral-Italic.ttf", weight: "400", style: "italic" },
+    { path: "../public/Spectral-Medium.ttf", weight: "500", style: "normal" },
+    { path: "../public/Spectral-MediumItalic.ttf", weight: "500", style: "italic" },
+    { path: "../public/Spectral-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "../public/Spectral-SemiBoldItalic.ttf", weight: "600", style: "italic" },
+  ],
   variable: "--font-spectral",
 });
 
